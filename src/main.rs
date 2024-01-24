c2503e686accc7757a90656fc5c599c9e5742df6
KA37RI <doubsing0125@gmail.com>
2024-01-25 01:16:10 +0900

    added framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct Args {
+  #[command(subcommand)]
+  command: Subcommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Subcommands {
+  #[command(about = "generate random test programs")]
+  Gen {
+    problem: String,
+  },
+  #[command(about = "execute random test")]
+  Test {
+    problem: String,
+  },
+}
+
+fn gen(problem: String) {
+  //
+}
+
+fn test(problem: String) {
+  //
+}
+
 fn main() {
-    println!("Hello, world!");
+  let arg = Args::parse();
+  match arg.command {
+    Subcommands::Gen { problem } => {
+      gen(problem);
+    },
+    Subcommands::Test { problem } => {
+      test(problem);
+    },
+  }
 }
