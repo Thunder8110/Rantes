f0f3dca407a6aae1eb9878d23ad0cc196b7abc0c
KA37RI <doubsing0125@gmail.com>
2024-01-25 17:01:05 +0900

    target directory changed to src/bin

--- src/gen.rs
+++ src/gen.rs
@@ -9,21 +9,21 @@ pub fn gen(problem: String) -> anyhow::Result<()> {
     bail!("Cargo.toml does not exists.");
   }
 
-  let rantes_dir_path = curr_dir_path.join("rantes");
-  match rantes_dir_path.try_exists() {
+  let src_bin_path = curr_dir_path.join("src").join("bin");
+  match src_bin_path.try_exists() {
     Ok(true) => Ok(()),
-    Ok(false) => fs::create_dir(&rantes_dir_path),
+    Ok(false) => fs::create_dir(&src_bin_path),
     Err(e) => Err(e),
   }?;
 
-  let problem_gen_path = rantes_dir_path.join(format!("{}_gen.rs", &problem));
+  let problem_gen_path = src_bin_path.join(format!("{}_gen.rs", &problem));
   match problem_gen_path.try_exists() {
     Ok(true) => bail!("generator for {} is exists.", &problem),
     Ok(false) => fs::File::create(&problem_gen_path),
     Err(e) => Err(e),
   }?;
   
-  let solve_slow_path = rantes_dir_path.join(format!("{}_slow.rs", &problem));
+  let solve_slow_path = src_bin_path.join(format!("{}_slow.rs", &problem));
   match solve_slow_path.try_exists() {
     Ok(true) => bail!("slow code for {} is exists.", &problem),
     Ok(false) => fs::File::create(&solve_slow_path),
